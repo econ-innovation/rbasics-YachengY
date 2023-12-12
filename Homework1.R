5a09311d47c641730b4d50aabc52217b41876e3e
Yacheng Yang <152443272+YachengY@users.noreply.github.com>
2023-12-12 23:36:32 +0800

    Add files via upload

---
+++ Homework1.R
@@ -0,0 +1,14 @@
+## Class Exercise 1
+as.character(3.141592657)
+as.logical(3.141592657)
+is.character("hello world")
+is.na(NA)
+v1 <- c(1,2,3,4)
+v1[c(T,F,T,F)]
+a <-v1%/%3
+
+v1[a] 
+v1[-1] 
+v1[5] 
+v1[] <- 99 
+v1 <- 99 
